82943ca47b17410b5547e044665540f42f8a33c4
David Charles <charled1@gmatc.matc.edu>
2022-09-06 16:49:12 -0500

    Format completed

---
+++ Charles_Assignment2/Main.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Charles_Assignment2;
+
+Employee employee;
+View v = new View();
+mainGo();
+
+void mainGo()
+{
+    string fName;
+    string lName;
+    double sales;
+
+    //gather information
+    v.welcome();
+    v.pleaseEnter("your first name");
+    fName = v.inString();
+    v.pleaseEnter("your last name");
+    lName = v.inString();
+    v.pleaseEnter("your sales for the month");
+    sales = Double.Parse(v.inString());
+
+    //create employee
+    employee = new Employee(fName,lName,sales);
+
+    //calculations
+    calculate();
+
+    //output
+    v.printString(employee.formattedOutput());
+}
+
+void calculate()
+{
+    employee.calcCommission();
+    employee.calcFedTax();
+    employee.calcSocialTax();
+    employee.calcRetirement();
+}
---
+++ Charles_Assignment2/View.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charles_Assignment2
+{
+    internal class View
+    {
+        public void welcome() {
+            Console.WriteLine("Welcome to Payroll Calculator!");
+            DisplayInfo("Assignment 2");
+        }
+
+        public void pleaseEnter(string whatToEnter) {
+            Console.WriteLine("Please enter " + whatToEnter + " :");
+        }
+
+        public string inString() {
+            return Console.ReadLine();
+        }
+
+        internal void printString(string m)
+        {
+            Console.WriteLine(m);
+        }
+
+        private void DisplayInfo(string currentAssignment)
+        {
+            string barrier = "***********************************************************************************";
+
+            Console.WriteLine(barrier + "\n\n");
+            Console.WriteLine("Name:\t\tDavid Charles");
+            Console.WriteLine("Course:\t\tITDEV-115-501");
+            Console.WriteLine("Instructor:\tMr. Menzl");
+            Console.WriteLine("Assignment:\t" + currentAssignment);
+            Console.WriteLine("Date:\t\t" + System.DateTime.Today.ToShortDateString());
+            Console.WriteLine("\n\n" + barrier);
+        }
+    }
+}
