ccdb7a3fbbc8e5257265f4c540a4b77563be83ad
David Charles <charled1@gmatc.matc.edu>
2022-09-12 10:56:08 -0500

    Started Assignment 3

---
+++ Charles_Assignment3/BMI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charles_Assignment3
+{
+    internal class BMI
+    {
+        //Class properties (Auto Implemented)
+        public double BodyMassIndex { get; set; } = 0.0;
+        public double HeightInches { get; set; } = 0.0;
+        public double WeightPounds { get; set; } = 0.0;
+
+        //Constructors
+        public BMI(double height, double wieght) { 
+            WeightPounds = wieght;
+            HeightInches = height;
+        }
+        public BMI() { 
+        
+        }
+
+        public void CalculateBMI() {
+            BodyMassIndex = (WeightPounds / (HeightInches * HeightInches)) * 703.0;
+        }
+
+
+    }
+}
---
+++ Charles_Assignment3/BMIUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charles_Assignment3
+{
+    internal class BMIUI
+    {
+        BMI userBMI = new BMI();
+        public void ObtainWeightHeight()
+        {
+            Console.WriteLine("Please Enter your weight in pounds: ");
+            userBMI.WeightPounds = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please Enter your height in inches: ");
+            userBMI.HeightInches = Double.Parse(Console.ReadLine());
+        }
+    }
+}
