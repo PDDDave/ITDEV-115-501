39a1d29a7012fa7968216e51aeb78051430b44e3
David Charles <charled1@gmatc.matc.edu>
2022-09-30 12:07:01 -0500

    created shell methods for Election class

--- Charles_MidTerm_Election_Program/Election.cs
+++ Charles_MidTerm_Election_Program/Election.cs
@@ -8,5 +8,28 @@ namespace Charles_MidTerm_Election_Program
 {
     internal class Election
     {
+        private const int NUM_OF_CANDIDATES = 5;
+
+        public int GetNumCandidates() { 
+            return NUM_OF_CANDIDATES;
+        }
+
+        public void SetCandidateName(string name, int index) { 
+        
+        }
+
+        public void SetCandidateVotes(int votes, int index) { 
+        
+        }
+
+        public void GetCandidateName(int index) { }
+
+        public void GetCandidateVotes(int index) { }
+
+        public void FindWinnder() { }
+
+        public void TotalVotes() { }
+
+        
     }
 }
