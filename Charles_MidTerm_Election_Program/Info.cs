89e0ec86c1b87344f67bb5034762d3be6a208f4d
David Charles <charled1@gmatc.matc.edu>
2022-09-30 11:51:56 -0500

    Midterm Classes created

---
+++ Charles_MidTerm_Election_Program/Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charles_MidTerm_Election_Program
+{
+    internal class Info
+    {
+    }
+}
