dfe8c9b9d8fb0d876e2e4d0f8dcd40af11f5d1e6
David Charles <charled1@gmatc.matc.edu>
2022-09-30 20:00:04 -0500

    Started Assignment 4

---
+++ TowersOfHanoi/Program.cs
@@ -0,0 +1,5 @@
+﻿using TowersOfHanoi;
+
+//geek to geek algorithm 
+Geek2Geek geek = new Geek2Geek();
+geek.AppGo(3);  //enter number of disks as int
---
+++ TowersOfHanoi/PythonTrans.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowersOfHanoi
+{
+    internal class PythonTrans
+    {
+
+        //this code is a translation from a video explaining The Towers of Hanoi problem: https://www.youtube.com/watch?v=rf6uf3jNjbo
+        public void AppGo() { 
+           
+        }
+    }
+}
