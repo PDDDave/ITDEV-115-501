0694d58e59b658427659e6a934e36814ae1e8e2d
David Charles <charled1@gmatc.matc.edu>
2022-10-31 15:56:14 -0500

    Basic structure in place

--- Charles_Assignment6/Farmer.cs
+++ Charles_Assignment6/Farmer.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Charles_Assignment6
 {
     internal class Farmer
     {
+        FarmerUI ui = new FarmerUI();
+        private bool cont = true;
+        private bool eaten = false;
+        private bool accross = false;
+        private ArrayList northBank = new ArrayList() { "farmer", "fox", "chicken", "grain"};
+        private ArrayList southBank = new ArrayList();
+
+
+        public void StartGame() {
+            ui.Welcome();
+            ui.DisplayScreen(northBank, southBank);
+            Console.ReadLine();
+            ui.PlayerChoice(northBank, southBank);
+            Console.ReadLine();
+
+           /* do {
+                ui.DisplayScreen(northBank, southBank);
+                GameOver();
+            }while(cont) */
+        }
+
+        private void GameOver() {
+        }
     }
 }
--- Charles_Assignment6/FarmerUI.cs
+++ Charles_Assignment6/FarmerUI.cs
@@ -1,12 +1,122 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Charles_Assignment6
 {
+    
     internal class FarmerUI
     {
+        Info info = new Info();
+
+        public void Welcome() {
+            info.DisplayInfo("Assignment 6");
+            Title();
+            Instructions();
+            Continue();
+        }
+
+        private void Ferry(string choice, ArrayList bank, string currentBank, string otherBank) {
+            WriteLine("Moving " + choice + " from " + currentBank + " to " + otherBank);
+        }
+
+        public void PlayerChoice(ArrayList northBank, ArrayList southBank) {
+            string choice = "";
+            string currentBank = ""; //true == north, false ==south
+            string otherBank = "";
+
+            if (northBank.Contains("farmer")) { 
+                currentBank = "north bank";
+                otherBank = "south bank";
+            } else { 
+                currentBank = "south bank";
+                otherBank = "north bank";
+            }
+            WriteLine("What should the Farmer take with him from the" + currentBank + " to the " + otherBank + "?:");
+            choice = ReadLine();
+            choice = choice.ToLower();
+
+            if (currentBank.Equals("north bank")) { //north=true
+                if (northBank.Contains(choice)) {
+                    Ferry(choice, northBank, currentBank, otherBank);
+                } else {
+                    WriteLine(System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(choice)
+                        + " is not on this side of the river.");
+                }
+            }
+            else {
+                if (southBank.Contains(choice)) { 
+                    Ferry(choice, southBank, currentBank, otherBank);
+                } else { 
+                    WriteLine(System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(choice)
+                        + " is not on this side of the river.");                    
+                }
+            }
+
+
+        }
+
+        public void DisplayScreen(ArrayList northBank, ArrayList southBank) {
+            string nBank = "";
+            if(northBank.Count > 0) {
+                for (int i = 0; i < northBank.Count; i++) {
+                    nBank += northBank[i] + "\t";
+                }
+            } else { 
+                nBank = " ";
+            }
+
+            string water = "";
+            for (int i = 0; i < 4; i++) { 
+                water += "^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ \n";
+            }
+
+            string sBank = "";
+            if( southBank.Count > 0 ) { 
+                for (int i = 0; i < southBank.Count; i++) { 
+                    sBank += southBank[i] + "\t";
+                }
+
+            } else {
+                sBank = " ";
+            }
+
+
+            BackgroundColor = ConsoleColor.DarkGreen;
+            WriteLine("\n"+nBank.PadRight(21)+"\n");
+            BackgroundColor = ConsoleColor.Blue;
+            WriteLine(water.PadRight(21));
+            BackgroundColor = ConsoleColor.DarkGreen;
+            WriteLine(sBank.PadRight(21)+"\n");
+            BackgroundColor= ConsoleColor.Black;
+        }
+
+        public void Continue() {
+            WriteLine("Press enter to continue...");
+            ReadLine();        
+        }
+
+        private void Instructions() {
+            string instructions = "The farmer must get his fox, chicken, and grain safley facross the river from the NORTH BANK to the SOUTH BANK " +
+                "The farmer can make the trip carying only one thing with him.  Choose which item you want the farmer to carry with him, and get all" +
+                "three across safley!";
+
+            WriteLine(instructions);
+        }
+
+        private void Title() {
+            string title = "\r\n  ___                          ___ _    _    _              ___           ___          _      " +
+                "\r\n | __|_ _ _ _ _ __  ___ _ _   / __| |_ (_)__| |_____ _ _   | __|____ __  / __|_ _ __ _(_)_ _  \r\n | _/ _` |" +
+                " '_| '  \\/ -_) '_| | (__| ' \\| / _| / / -_) ' \\  | _/ _ \\ \\ / | (_ | '_/ _` | | ' \\ \r\n |_|\\__,_|_| |_|_" +
+                "|_\\___|_|    \\___|_||_|_\\__|_\\_\\___|_||_| |_|\\___/_\\_\\  \\___|_| \\__,_|_|_||_|\r\n " +
+                "                                                                                             \r\n";
+            WriteLine(title);
+        }
     }
 }
