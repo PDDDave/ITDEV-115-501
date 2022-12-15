227ea73bdf3b9f03232ad7801f8fdf9d51bed650
David Charles <charled1@gmatc.matc.edu>
2022-12-14 18:47:55 -0600

    Basic structure in place

--- Charles_Final/GradesUI.cs
+++ Charles_Final/GradesUI.cs
@@ -8,5 +8,27 @@ namespace Charles_Final
 {
     internal class GradesUI
     {
+        StudentS myStudentS;
+
+        public void MainMethod()
+        {
+            //instance an object of StudentUI..
+            //Call StudentUI->PopulateStudents..
+            //Verify file was successfully read.
+            //if successfull call DisplayInfo()
+            //else display error message.
+
+        }
+
+        void DisplayInfo()
+        {
+            Console.WriteLine("Student id\tLast Name\tAverage  \tGrade");
+
+            for (int index = 0; index < myStudentS.ListLength; index++)
+            {
+
+                Console.WriteLine(" {0} \t {1}    \t {2}    \t {3}", myStudentS.StudentID(index), myStudentS.StudentLastName(index), myStudentS.StudentAverage(index), myStudentS.StudentGrade(index));
+            }
+        }
     }
 }
